2f7c045fe167c55a3285e6ce2d5a57c88799ce99
SURYA <107025881+001surya011@users.noreply.github.com>
2023-01-20 11:30:37 +0530

    Update PORTFOLIO1.py

--- PORTFOLIO1.py
+++ PORTFOLIO1.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, url_for, request, redirect
 import csv
 app = Flask(__name__)
 
-print(__name__)
-
 @app.route('/')
 def index_page():
     return render_template('index.html')
@@ -11,34 +9,6 @@ def index_page():
 @app.route('/<string:page_name>') # Dynamically One Liner For Working.
 def html_page(page_name):
     return render_template(page_name)
-
-
-# @app.route('/home.html')
-# def home():
-#     return render_template('components.html')
-#
-# @app.route('/contact.html')
-# def contact():
-#     return render_template('contact.html')
-#
-# @app.route('/works.html')
-# def works():
-#     return render_template('works.html')
-#
-# @app.route('/about.html')
-# def about():
-#     return render_template('about.html')
-#
-# @app.route('/thankyou.html')
-# def thank():
-#     return render_template('thankyou.html')
-
-def write_to_file(data):
-    with open('Database.txt', mode='a') as database1:
-        email = data["email"] 
-        subject = data["subject"]
-        message = data["message"]
-        file = database1.write(f'\n Email :{email}; Subject :{subject}; Message:{message}')
 
 def write_to_csv(data): # csv --> Comma Separated Values
     with open('Database.csv', mode='a', newline='') as database2:
@@ -48,7 +18,6 @@ def write_to_csv(data): # csv --> Comma Separated Values
         # csv_writer.writeheader()
         csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
         csv_writer.writerow([email,subject,message]) # List Format Is Important
-
 
 @app.route('/submit_form', methods = ['POST', 'GET'])
 def submit_form():
